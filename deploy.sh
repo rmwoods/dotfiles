fad7692d0c3cdbed75219beb981962b0117c08af
Rory Woods <rory@preteckt.com>
2018-01-23 11:28:38 -0500

    Fixed interpreter, added bashrc linking.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,21 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # This script sym-links all of the configuration files in this directory to the location they should be
 
+PWD=`pwd`
+BASHRC=~/.bashrc
+ALIASES=~/.bash_aliases
 
+backup(){
+    if [ -e $1 ]
+    then
+        mv $1 $1.bak
+    fi
+}
+
+# bash
+backup $BASHRC
+backup $ALIASES
+
+ln -s $PWD/bashrc $BASHRC
+ln -s $PWD/bash_aliases $ALIASES
